ad3875c892c7601cc67491ba3e645d865005c018
Simonwep <toports@gmx.de>
2018-12-31 20:18:17 +0100

    Update dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
     devServer: {
         contentBase: __dirname + '/',
         host: '0.0.0.0',
-        port: 8080
+        port: 3005
     },
 
     module: {
@@ -34,12 +34,7 @@ module.exports = {
                 test: /\.scss$/,
                 use: [
                     MiniCssExtractPlugin.loader,
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            minimize: true
-                        }
-                    },
+                    'css-loader',
                     'sass-loader'
                 ]
             }

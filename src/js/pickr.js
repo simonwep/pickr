01db62d1af8dde81143fe4afa28dcbb3b5506264
Kevin Eshleman <hello@redkevin.com>
2019-05-29 17:28:58 -0500

    #91 - neglected to update the Pickr.version

--- src/js/pickr.js
+++ src/js/pickr.js
@@ -222,7 +222,7 @@ class Pickr {
                     const cssRGBaString = _color.toRGBA().toString();
                     this.element.style.background = cssRGBaString;
                     this.wrapper.style.background = `
-                        linear-gradient(to top, rgba(0, 0, 0, ${_color.a}), transparent), 
+                        linear-gradient(to top, rgba(0, 0, 0, ${_color.a}), transparent),
                         linear-gradient(to left, hsla(${_color.h}, 100%, 50%, ${_color.a}), rgba(255, 255, 255, ${_color.a}))
                     `;
 
@@ -756,5 +756,5 @@ Pickr.utils = _;
 Pickr.create = options => new Pickr(options);
 
 // Assign version and export
-Pickr.version = '0.6.1';
+Pickr.version = '0.6.2';
 export default Pickr;

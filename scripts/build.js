c3d4be1f1070e9c7eba588287d7e9de5c1327c94
Peter Freeze <pjfreeze@users.noreply.github.com>
2019-06-04 13:15:21 -0500

    Use a promise chain to build each file preventing multiple write handles

--- scripts/build.js
+++ scripts/build.js
@@ -7,79 +7,89 @@ const chalk = require('chalk');
 const path = require('path');
 const fs = require('fs');
 
-for (const {filename, babelConfig} of bundles) {
-    console.log(chalk.yellow(`[START] Build ${filename}...`));
+// Chaining promises to prevent issues caused by both filename configurations
+// writing a minified CSS file; both processes having handles on the files can
+// result in strange suffixes that fail to parse due to an extra `ap*/`
+bundles.reduce((memo, { filename, babelConfig }) => {
+    memo = memo.then(() => {
+        return new Promise((resolve, reject) => {
+            console.log(chalk.yellow(`[START] Build ${filename}...`));
 
-    webpack({
-        entry: path.resolve('./src/js/pickr.js'),
+            webpack({
+                entry: path.resolve('./src/js/pickr.js'),
 
-        mode: 'production',
+                mode: 'production',
 
-        output: {
-            path: path.resolve('./dist'),
-            filename,
-            library: 'Pickr',
-            libraryExport: 'default',
-            libraryTarget: 'umd'
-        },
+                output: {
+                    path: path.resolve('./dist'),
+                    filename,
+                    library: 'Pickr',
+                    libraryExport: 'default',
+                    libraryTarget: 'umd'
+                },
 
-        module: {
-            rules: [
-                {
-                    test: /\.js$/,
-                    use: [
+                module: {
+                    rules: [
                         {
-                            loader: 'babel-loader',
-                            options: babelConfig
+                            test: /\.js$/,
+                            use: [
+                                {
+                                    loader: 'babel-loader',
+                                    options: babelConfig
+                                },
+                                'eslint-loader',
+                                'thread-loader'
+                            ]
                         },
-                        'eslint-loader',
-                        'thread-loader'
+                        {
+                            test: /\.scss$/,
+                            use: [
+                                MiniCssExtractPlugin.loader,
+                                'css-loader',
+                                'sass-loader',
+                                'thread-loader'
+                            ]
+                        }
                     ]
                 },
-                {
-                    test: /\.scss$/,
-                    use: [
-                        MiniCssExtractPlugin.loader,
-                        'css-loader',
-                        'sass-loader',
-                        'thread-loader'
-                    ]
+
+                plugins: [
+                    new MiniCssExtractPlugin({
+                        filename: 'pickr.min.css'
+                    }),
+
+                    new OptimizeCssAssetsPlugin({
+                        cssProcessorPluginOptions: {
+                            preset: ['default', {discardComments: {removeAll: true}}]
+                        }
+                    }),
+
+                    new webpack.SourceMapDevToolPlugin({
+                        filename: `${filename}.map`
+                    }),
+
+                    new webpack.BannerPlugin({
+                        banner: `Pickr ${version} MIT | https://github.com/Simonwep/pickr`
+                    })
+                ]
+            }, (err, stats) => {
+                if (err || stats.hasErrors()) {
+                    console.error(err, stats);
+                    process.exit(-1);
+                    reject();
                 }
-            ]
-        },
+                resolve();
 
-        plugins: [
-            new MiniCssExtractPlugin({
-                filename: 'pickr.min.css'
-            }),
-
-            new OptimizeCssAssetsPlugin({
-                cssProcessorPluginOptions: {
-                    preset: ['default', {discardComments: {removeAll: true}}]
-                }
-            }),
-
-            new webpack.SourceMapDevToolPlugin({
-                filename: `${filename}.map`
-            }),
-
-            new webpack.BannerPlugin({
-                banner: `Pickr ${version} MIT | https://github.com/Simonwep/pickr`
-            })
-        ]
-    }, (err, stats) => {
-        if (err || stats.hasErrors()) {
-            console.error(err, stats);
-            process.exit(-1);
-        }
-
-        const oo = stats.compilation.outputOptions;
-        const fstats = fs.statSync(path.resolve(oo.path, oo.filename));
-        const bundleSize = readableByteSize(fstats.size);
-        const diff = stats.endTime - stats.startTime;
-        console.log(chalk.green(`[SUCCESS] Build of ${filename} took ${diff}ms ${chalk.magenta(`(${bundleSize})`)}`));
+                const oo = stats.compilation.outputOptions;
+                const fstats = fs.statSync(path.resolve(oo.path, oo.filename));
+                const bundleSize = readableByteSize(fstats.size);
+                const diff = stats.endTime - stats.startTime;
+                console.log(chalk.green(`[SUCCESS] Build of ${filename} took ${diff}ms ${chalk.magenta(`(${bundleSize})`)}`));
+            });
+        });
     });
-}
+    return memo;
+}, Promise.resolve());
 
 function readableByteSize(bytes, mapValue = v => v) {
     const si = false;

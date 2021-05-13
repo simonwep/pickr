83fdaf52ee9c43cad477b3ed247cfb9e37025165
Simon R <simon@reinisch.io>
2021-05-13 11:09:56 +0200

    Update types for i18n
    Fixes #274

--- types/pickr.d.ts
+++ types/pickr.d.ts
@@ -91,10 +91,21 @@ declare namespace Pickr {
             };
         };
 
-        strings?: {
-            save?: string;
-            clear?: string;
-            cancel?: string;
+        i18n?: {
+            'ui:dialog'?: string;
+            'btn:toggle'?: string;
+            'btn:swatch'?: string;
+            'btn:last-color'?: string;
+            'btn:save'?: string;
+            'btn:cancel'?: string;
+            'btn:clear'?: string;
+            'aria:btn:save'?: string;
+            'aria:btn:cancel'?: string;
+            'aria:btn:clear'?: string;
+            'aria:input'?: string;
+            'aria:palette'?: string;
+            'aria:hue'?: string;
+            'aria:opacity'?: string;
         }
     }
 

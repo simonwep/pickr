366129579bbcfc2b3f0cbcd004cb33e5e2a69cbd
Simon R <simon@reinisch.io>
2021-06-27 11:19:08 +0200

    Fix dev server

--- webpack.config.js
+++ webpack.config.js
@@ -4,23 +4,24 @@ const webpack = require('webpack');
 
 module.exports = {
     entry: {
-        'pickr.es5.min': './src/js/pickr.js',
-        'themes/classic.min': './src/scss/themes/classic.scss',
-        'themes/nano.min': './src/scss/themes/nano.scss',
-        'themes/monolith.min': './src/scss/themes/monolith.scss'
+        'dist/pickr.es5.min': './src/js/pickr.js',
+        'dist/themes/classic.min': './src/scss/themes/classic.scss',
+        'dist/themes/nano.min': './src/scss/themes/nano.scss',
+        'dist/themes/monolith.min': './src/scss/themes/monolith.scss'
     },
 
     output: {
-        publicPath: 'dist',
         filename: '[name].js',
-        library: 'Pickr',
-        libraryExport: 'default',
-        libraryTarget: 'umd'
+        library: {
+            type: 'umd',
+            name: 'Pickr',
+            export: 'default',
+            umdNamedDefine: true
+        }
     },
 
     devServer: {
-        contentBase: `${__dirname}/`,
-        disableHostCheck: true,
+        static: '.',
         host: '0.0.0.0',
         port: 3006
     },
